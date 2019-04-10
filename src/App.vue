2ed8fc70e1d720dd93a84c0746d899fbe7333e20
Jose Guerrero Roman <joseguerreroroman@MacBook-Pro-de-Jose.local>
2019-04-10 18:29:20 -0500

    Adding new components and table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app dark>
+    <v-toolbar fixed clipped-left class="deep-purple lighten-1" app>
+      <v-toolbar-title class="headline">
+        <span>Ingenieria MEP</span>
+        <span class="pl-5 ml-5 display-1">Proyectos</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn
+          flat
+          href="https://www.ingenieriamep.mx/"
+          target="_self"
+        >
+          <span v-ripple class="mr-2">Logout</span>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+
+      <UserTable />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
+import UserTable from './components/UserTable'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    UserTable
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/UserTable.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-container>
+      <v-data-table
+        :headers="headers"
+        :items="users"
+        class="elevation-1"
+      >
+        <template v-slot:items="props">
+          <td>{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.calories }}</td>
+          <td class="text-xs-right">{{ props.item.fat }}</td>
+          <td class="text-xs-right">{{ props.item.carbs }}</td>
+          <td class="text-xs-right">{{ props.item.protein }}</td>
+          <td class="text-xs-right">{{ props.item.iron }}</td>
+        </template>
+      </v-data-table>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'UserTable',
+  data () {
+    return {
+      headers: [
+        { text: 'Usuario',
+          align: 'left',
+          value: 'userName'
+        },
+        { text: 'Contraseña',
+          sortable: false,
+          value: 'password'
+        },
+        { text: 'Repositorio(s)',
+          value: 'links'
+        }
+      ],
+      users: []
+    }
+  }
+}
+</script>
